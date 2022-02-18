d9c6c66f68eabfa7b124ca399ae54b3a9411d7bc
Erick <emgy1995@gmail.com>
2022-02-18 17:57:14 +0800

    testing paginated content load, firebase mock data updated

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,19 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import { useEffect, createContext, useContext, useRef } from "react";
-import Loader from "../components/Loader";
-import { postToJSON } from "../lib/firebase";
+import { Loader, PostFeed } from "../components/Loader";
+import { firestore, fromMillis, postToJSON } from "../lib/firebase";
 
 const LIMIT = 1;
 export async function getServersideProps(context) {
   const postsQuery = firestore
     .collectionGroup("posts")
     .where("published", "==", true)
-    .orderby("createdAt", "desc")
+    .orderBy("createdAt", "desc")
     .limit(LIMIT);
 
   const posts = (await postsQuery.get()).docs.map(postToJSON);
+
   return {
     props: { posts },
   };
@@ -26,40 +27,51 @@ export default function Home(props) {
   const [postsEnd, setPostsEnd] = useState(false);
 
   const getMorePosts = async () => {
-    setLoading(true);
-    const last = posts[posts.length - 1];
+    // setLoading(true);
+    // const last = posts[posts.length - 1];
 
-    const cursor =
-      typeof last.createdAt === "number"
-        ? fromMillis(last.createdAt)
-        : last.createdAt;
+    // const cursor =
+    //   typeof last.createdAt === "number"
+    //     ? fromMillis(last.createdAt)
+    //     : last.createdAt;
 
-    const query = firestore
+    // const query = firestore
+    //   .collectionGroup("posts")
+    //   .where("published", "==", true)
+    //   .orderBy("createdAt", "desc")
+    //   .startAfter(cursor)
+    //   .limit(LIMIT);
+
+    // const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    // setPosts(posts.concat(newPosts));
+    // setLoading(false);
+
+    // if (newPosts.length < LIMIT) {
+    //   setPostsEnd(true);
+    // }
+    const postsQuery = firestore
       .collectionGroup("posts")
       .where("published", "==", true)
       .orderBy("createdAt", "desc")
-      .startAfter(cursor)
       .limit(LIMIT);
 
-    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+    const incomingPost = (await postsQuery.get()).docs.map(postToJSON);
 
-    setPosts(posts.concat(newPosts));
-    setLoading(false);
-
-    if (newPosts.length < LIMIT) {
-      setPostsEnd(true);
-    }
+    console.log(incomingPost);
   };
 
   return (
     <main>
-      <PostFeed posts={posts} />
+      {/* <PostFeed posts={posts} />
       {!loading && !postsEnd && (
         <button onClick={getMorePosts}>Load More</button>
       )}
       <Loader show={loading}></Loader>
 
-      {postsEnd && "You have reached the end!"}
+      {postsEnd && "You have reached the end!"} */}
+      <PostFeed posts={posts} />
+      <button onClick={getMorePosts}>Load More</button>
     </main>
   );
 }
